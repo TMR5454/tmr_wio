dad145378a1fd05d35964bf1d27ce4224c5b6111
Tamura Dai <kirinode0@gmail.com>
2023-12-16 10:49:52 +0900

    add button2 & button3

--- examples/button_led.rs
+++ examples/button_led.rs
@@ -8,7 +8,7 @@ use wio_terminal as wio;
 use wio::entry;
 use wio::pac::Peripherals;
 
-use tmr_wio::button::Button;
+use tmr_wio::button::*;
 use tmr_wio::led::Led;
 
 #[entry]
@@ -16,13 +16,12 @@ fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let mut pins = wio::Pins::new(peripherals.PORT);
     let mut led = Led::new(pins.user_led, &mut pins.port);
-    let button1 = Button::new(pins.button1, &mut pins.port);
-    //    let button2 = Button::new(pins.button2, &mut pins.port);
-    //    let button3 = Button::new(pins.button3, &mut pins.port);
+    let button1 = Button1::new(pins.button1, &mut pins.port);
+    let button2 = Button2::new(pins.button2, &mut pins.port);
+    let button3 = Button3::new(pins.button3, &mut pins.port);
 
     loop {
-        //        if button1.is_pressed() || button2.is_pressed() || button3.is_pressed() {
-        if button1.is_pressed() {
+        if button1.is_pressed() || button2.is_pressed() || button3.is_pressed() {
             led.turn_on();
         } else {
             led.turn_off();
--- src/button.rs
+++ src/button.rs
@@ -3,22 +3,73 @@ use wio_terminal as wio;
 use wio::hal::gpio::*;
 use wio::prelude::*;
 
-pub struct Button {
+pub trait Button {
+    fn is_pressed(&self) -> bool;
+    fn is_released(&self) -> bool;
+}
+
+pub struct Button1 {
     pin: Pc26<Input<Floating>>,
 }
 
-impl Button {
+impl Button1 {
     pub fn new(pin: Pc26<Input<Floating>>, port: &mut Port) -> Self {
         Self {
             pin: pin.into_floating_input(port),
         }
     }
+}
 
-    pub fn is_pressed(&self) -> bool {
+impl Button for Button1 {
+    fn is_pressed(&self) -> bool {
         self.pin.is_low().unwrap()
     }
 
-    pub fn is_released(&self) -> bool {
+    fn is_released(&self) -> bool {
+        self.pin.is_high().unwrap()
+    }
+}
+
+pub struct Button2 {
+    pin: Pc27<Input<Floating>>,
+}
+
+impl Button2 {
+    pub fn new(pin: Pc27<Input<Floating>>, port: &mut Port) -> Self {
+        Self {
+            pin: pin.into_floating_input(port),
+        }
+    }
+}
+
+impl Button for Button2 {
+    fn is_pressed(&self) -> bool {
+        self.pin.is_low().unwrap()
+    }
+
+    fn is_released(&self) -> bool {
+        self.pin.is_high().unwrap()
+    }
+}
+
+pub struct Button3 {
+    pin: Pc28<Input<Floating>>,
+}
+
+impl Button3 {
+    pub fn new(pin: Pc28<Input<Floating>>, port: &mut Port) -> Self {
+        Self {
+            pin: pin.into_floating_input(port),
+        }
+    }
+}
+
+impl Button for Button3 {
+    fn is_pressed(&self) -> bool {
+        self.pin.is_low().unwrap()
+    }
+
+    fn is_released(&self) -> bool {
         self.pin.is_high().unwrap()
     }
 }
